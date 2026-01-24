457e7eee6073249dc4275f39127f6760f23842a2
VishSeran <vishwa.sovis@gmail.com>
2026-01-24 14:24:33 +0530

    try, except and finally exercise 1

--- exception_handling.py
+++ exception_handling.py
@@ -9,4 +9,17 @@ else:
     
     print("Successfull!")
 
-    
+
+import math
+#Type your code here
+def safe_root(number):
+    try:
+        result = math.sqrt(number)
+        return result
+    except ValueError:
+        print('Invalid input! Please enter a positive integer or a float value.')
+    finally:
+        print('Result: ', result)
+
+safe_root(5)
+safe_root(-5)
