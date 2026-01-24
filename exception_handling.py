1542f2beadd22b32fded311215b881c1db804ece
VishSeran <vishwa.sovis@gmail.com>
2026-01-24 14:36:29 +0530

    try, except and finally exercise 2

--- exception_handling.py
+++ exception_handling.py
@@ -8,8 +8,23 @@ except ZeroDivisionError:
 else:
     
     print("Successfull!")
+    
+
+##ZeroDivisionError
+def safe_divide(numerator,denominator):
+    try:
+        result = numerator / denominator
+        return result
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+        return None
+# Test case
+numerator=int(input("Enter the numerator value:-"))
+denominator=int(input("Enter the denominator value:-"))
+print(safe_divide(numerator,denominator))
 
 
+##ValueError
 import math
 #Type your code here
 def safe_root(number):
@@ -23,3 +38,19 @@ def safe_root(number):
 
 safe_root(5)
 safe_root(-5)
+
+##Handling Generic Exception
+def complex_maths(number):
+    
+    try:
+        result = result / (result-5)
+        return result
+
+    except Exception as e:
+        print("An error occurred during calculation.")
+
+user_input = float(input('Enter a number: '))
+complex_maths(user_input)
+
+complex_maths(2)
+        
