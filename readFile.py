5fa1573917e05e56b5846d993056f0b12af0af06
VishSeran <vishwa.sovis@gmail.com>
2026-02-17 18:46:31 +0530

    read from a file

---
+++ readFile.py
@@ -0,0 +1,42 @@
+##dierct read from a file but we need to close the file object 
+""" fileSeran = open("text.txt","r")
+fileSeran.name
+fileSeran.mode 
+
+fileStuff = fileSeran.read()
+print(fileStuff)
+
+fileSeran.close() """
+
+## using with method it is automatically close the file object
+##with open("text.txt","r") as file1:
+    ##fileDetails = file1.read()
+    ##fileLines = file1.readlines()
+    ##print(fileDetails)
+    ##print(fileLines)
+    
+    ##fileLine = file1.readline()
+    ##print(fileLine)
+    
+   ## fileLine = file1.readline()
+    ##print(fileLine)
+    ##fileLine = file1.readline()
+   ## print(fileLine) """
+    
+with open("text.txt","r") as file1:
+    line1 = file1.readline()
+    line2 = file1.readline()
+    line3 = file1.readline()
+    
+    print(line1)
+    if "important" in line2:
+        print(line2)
+    else:
+        print('nothing')
+        
+    if "important" in line3:
+        print(line3)
+    else:
+        print("nothing")
+
+
